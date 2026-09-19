3690687c4240000c5d44abcfbb32768290569b61
Marcelo Maurin Martins <marcelomaurinmartins@gmail.com>
2026-09-19 13:51:48 -0300

    audit: registra logout

--- apps/web/logout.php
+++ apps/web/logout.php
@@ -1,5 +1,7 @@
 <?php
 require __DIR__ . '/lib/bootstrap.php';
+$u=current_user();
+if($u) audit_log('auth.logout','user',$u['id']);
 $_SESSION = [];
 session_destroy();
 header('Location: login.php');
